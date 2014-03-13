b424bf13754c9b159baf51f3f75289d066a9f2de
Tomas Barton <barton.tomas@gmail.com>
2014-03-13 10:37:08 +0100

    munge spec

---
+++ spec/classes/munge_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'torque::munge' do
+
+  let(:facts) {{
+    :operatingsystem => 'Debian',
+    :osfamily        => 'Debian',
+    :lsbdistcodename => 'wheezy',
+    :lsbdistid       => 'Debian',
+  }}
+
+  it { should contain_class('torque::munge') }
+  it { should contain_package('munge').with(
+        :ensure => 'installed',
+  )}
+
+  it { should contain_service('munge').with(
+        :ensure => 'running',
+        :enable => true
+  )}
+
+  it {
+    should contain_file(
+      '/etc/munge/munge.key'
+    ).with({
+    'ensure'  => 'present',
+    'owner'   => 'munge',
+    'group'   => 'munge',
+    'mode'    => '0400',
+    })
+  }
+
+end
